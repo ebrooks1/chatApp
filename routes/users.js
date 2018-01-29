4f15625a69f928bba2aeb7ba560e760f2ac4f23e
Brooks <e_brooks@tss-hr420-dm09.fc.ca>
2018-01-29 12:20:03 -0500

    updating structure

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express'); //this is the same as a php include
+const router = express.Router();
+const path = require('path');
+
+router.get('/users', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../views/users.html'))
+});
+
+module.exports = router;
