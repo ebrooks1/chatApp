8b13751b0320ece6c3218841f48df23141206862
Brooks <e_brooks@tss-hr420-dm09.fc.ca>
2018-01-29 13:27:29 -0500

    added the grunt stuff

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
+const io = require('socket.io')();
+
+app.use(express.static('public'));
 
 // this is a route. this points at the home page / root
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-})
+//set up routes
+app.use(require('./routes/index'));
+app.use(require('./routes/users'));
+app.use(require('./routes/contact'));
 
-// set up a contact route
-app.get('/contact', (req, res) => {
-  res.sendFile(__dirname + '/contact.html');
-})
-
-// set up a portfolio route
-app.get('/portfolio', (req, res) => {
-  res.sendFile(__dirname + '/portfolio.html');
-})
-
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('app running on port 3000!');
-})
+});
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+  console.log('a user has connected!');
+  io.emit('connectMsg', { for: 'everyone', msg : `${socket.id} is here!`});
+
+  socket.on('disconnect', () => {
+    console.log('a user has disconnected!');
+  });
+
+});
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express'); //this is the same as a php include
 const router = express.Router();
 const path = require('path');
 
-router.get('/index', (req, res) => {
+router.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../views/index.html'))
 });
 
